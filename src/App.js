0475baeee3d98b733130be3188b0b4a3ef0d6b4b
Juan Correa <ema8720@gmail.com>
2023-07-15 08:29:26 -0700

    workshop 3

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
